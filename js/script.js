766d080263d92be13aa7abded8eb9e6f81638c04
Grégory Locigno <gregory.locigno@gmail.com>
2023-03-11 15:12:37 +0100

    test JSON file

--- js/script.js
+++ js/script.js
@@ -6,12 +6,8 @@ const memoryScore = document.getElementById('memory-score');
 const verbalScore = document.getElementById('verbal-score');
 const visualScore = document.getElementById('visual-score');
 
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const url = '/js/data.json';
-const apiUrl = proxyUrl + url;
-
 async function getJson() {
-  const response = await fetch(apiUrl);
+  const response = await fetch('/js/data.json');
   const data = await response.json();
   const reactionObject = data[0];
   const memoryObject = data[1];
